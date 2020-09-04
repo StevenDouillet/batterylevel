8113062958ca05f73ba2b856538faa21bd999640
DOUILLET <Steven.douillet@gmail.com>
2020-09-04 09:48:19 +0200

    get battery level

---
+++ app/src/main/java/com/example/batterylevel/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.batterylevel;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView contentTxt;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BatteryManager bm = (BatteryManager) this.getSystemService(BATTERY_SERVICE);
+        int batLevel = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+
+        contentTxt = (TextView) this.findViewById(R.id.battery_level);
+        contentTxt.setText(Integer.toString(batLevel));
+
+        this.registerReceiver(this.mBatInfoReceiver,
+                new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    }
+
+    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context arg0, Intent intent) {
+            int level = intent.getIntExtra("level", 0);
+            contentTxt.setText(String.valueOf(level) + "%");
+        }
+    };
+}
